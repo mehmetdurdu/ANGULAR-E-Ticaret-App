ce0c5d1842d7cba757d3308d993a84114445dd23
Mehmet <mehmetdurdu25@hotmail.com>
2022-07-01 15:48:41 +0300

    New E Ticaret App Project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { LoginGuard } from './login/login.guard';
+import { ProductAddForms1Component } from './product/product-add-forms1/product-add-forms1.component';
+import { ProductAddForms2Component } from './product/product-add-forms2/product-add-forms2.component';
+import { ProductComponent } from './product/product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'products', component: ProductComponent },
+  { path: 'product-add-1', component: ProductAddForms1Component, canActivate:[LoginGuard] },
+  { path: 'product-add-2', component: ProductAddForms2Component, canActivate:[LoginGuard] },
+  { path: '', redirectTo: 'products', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'products/category/:categoryId', component: ProductComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
